458fe9c027622a5398285fe04b657caabfd9a3fa
m3y <ma3ya.ozw+github@gmail.com>
2017-03-21 01:00:09 +0900

    first commit

---
+++ src/main/java/com/example/Person.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javax.persistence.*;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
